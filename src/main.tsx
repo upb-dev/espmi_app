ba074db432bd424821e6a5c5fcfc8700d7f5e808
Sarikhin <sarikhinlovestory@gmail.com>
2023-06-08 23:36:29 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { StyledEngineProvider } from "@mui/styled-engine";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "../src/app/App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+        <CssBaseline>
+          <App />
+        </CssBaseline>
+      </BrowserRouter>
+    </StyledEngineProvider>
+  </React.StrictMode>
+);
